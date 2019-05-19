dc59ec79314c23bc6794d06e0c6c77c81223f47f
mhigaki <milton.higaki@gmail.com>
2019-05-19 18:53:10 -0300

    update - diversos

--- js/contact-form-script.js
+++ js/contact-form-script.js
@@ -49,7 +49,7 @@ function submitMSG(valid, msg) {
   if (valid) {
     var msgClasses = "h3 text-center tada animated text-success";
   } else {
-    var msgClasses = "h3 text-center text-danger";
+    var msgClasses = "h3 text-center text-warning";
   }
   $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
 }
